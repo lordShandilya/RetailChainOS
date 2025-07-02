54c952d40528099dd94b26c5398366b4d41e58f3
lordShandilya <vshandilya220@gmail.com>
2025-07-02 22:49:17 +0530

    feat: fulfill-smart

---
+++ app/fulfill_smart/models/models.py
@@ -0,0 +1,32 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import relationship
+
+Base = declarative_base()
+
+class FulfillmentCenter(Base):
+    __tablename__ = 'FulfillmentCenter'
+
+    id = Column(String, primary_key = True)
+    latitude = Column(Float)
+    longitude = Column(Float)
+    current_workload = Column(Integer)
+    handling_capacity = Column(Integer)
+
+    inventory_items = relationship("InventoryItem", back_populates="fulfillment_center", cascade="all, delete-orphan")
+
+    def __repr__(self):
+        return f"<FulfillmentCenters(id={self.id}, location=[longitude: {self.longitude}, latitude: {self.latitude}], current_workload={self.current_workload}, handling_capacity={self.handling_capacity})>"
+    
+class InventoryItem(Base):
+    __tablename__ = 'InventoryItem'
+
+    id = Column(String, primary_key = True)
+    sku = Column(String)
+    quantity = Column(Integer)
+    fulfillment_center_id = Column(String, ForeignKey('FulfillmentCenter.id'))
+
+    fulfillment_center = relationship("FulfillmentCenter", back_populates="inventory_items")
+
+    def __repr__(self):
+        return f"<InventoryItem(id={self.id}, sku={self.sku}, quantity={self.quantity}, fulfillment_center_id={self.fulfillment_center_id})>"
